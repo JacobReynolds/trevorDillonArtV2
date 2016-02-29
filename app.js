9509a266b0c543f1daf26008fbaa92f19436ce34
pny <reyno511@umn.edu>
2016-02-29 16:04:55 -0600

    portfolio test

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ app.use(function (err, req, res, next) {
     });
 });
 
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
