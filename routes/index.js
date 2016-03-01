93f76b321a71aedcff2dc0dc6501143dd9226240
pny <reyno511@umn.edu>
2016-03-01 17:43:48 -0600

    trying new homepage

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,10 @@ router.get('/', function (req, res) {
     res.render('index');
 });
 
+router.get('/contact', function (req, res) {
+    res.render('contact');
+});
+
 //Creation of the publicPortfolioPath should be taken care of by .openshift/action_hooks/deploy
 if (!fs.existsSync(portfolioPath)) {
     fs.mkdirSync(portfolioPath);
