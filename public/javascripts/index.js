46b87199e518841b0180629e221d6c48e196112a
jasper <reyno511@umn.edu>
2016-03-25 21:47:45 -0500

    meta tag test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,6 @@
+//GoDaddy nests in a iframe which removes our meta tags, this will make sure to add them
+initializeMobile();
+
 $(document).ready(function () {
     //No need for the home screen to be highlighted
     if (window.location.pathname === '/') {
@@ -9,4 +12,12 @@ $(document).ready(function () {
 
 function addActive(section) {
     $('#navOptions #' + section).addClass("active");
+}
+
+function initializeMobile() {
+    var headTag = window.parent.document.getElementsByTagName('head');
+
+    if (headTag[0].innerHTML.indexOf('viewport') === -1) {
+        $(headTag).append('<meta name="viewport" content="width=device-width, initial-scale=1"/>');
+    }
 }
