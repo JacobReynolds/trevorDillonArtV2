55de1d86eccacc7afddd71807aa779ef2b37ddaf
jasper <reyno511@umn.edu>
2016-05-05 10:18:27 -0500

    adding loading

--- public/javascripts/portfolio.js
+++ public/javascripts/portfolio.js
@@ -5,12 +5,25 @@ var loadAmount = 10;
 var imagePath = 'images/portfolio/';
 
 function loadMore() {
-    imageAmount += loadAmount;
-    $.get('/portfolio', {
-        amount: imageAmount
-    }).success(function (data) {
-        data.forEach(function (image) {
-            $('#photos').append('<div id="advancedImageContainer" onclick="enlargePicture(this);"><img src="' + imagePath + image + '"/></div>');
-        })
-    })
+	imageAmount += loadAmount;
+	$.get('/portfolio', {
+		amount: imageAmount
+	}).success(function (data) {
+		if (data.length < loadAmount) {
+			$('#loadWrapper').css('display', 'none');
+		}
+		data.forEach(function (image) {
+			$('#photos').append('<div id="advancedImageContainer" onclick="enlargePicture(this);"><img src="' + imagePath + image + '"/></div>');
+		})
+	})
 }
+
+$(window).on('load', function () {
+	$('#photos').css('display', 'block');
+	$('#description').css('display', 'block');
+	$('#loadWrapper').css('display', 'block');
+	$('#waitWrapper').css('display', 'none');
+	if ($('#photos div').length < loadAmount) {
+		$('#loadWrapper').css('display', 'none');
+	}
+})
--- public/javascripts/recent.js
+++ public/javascripts/recent.js
@@ -2,30 +2,36 @@ var previousImage = null;
 
 
 function loadInsta() {
-    "use strict";
-    $.ajax({
-        type: "GET",
-        dataType: "jsonp",
-        cache: false,
-        url: "https://api.instagram.com/v1/users/self/media/recent?access_token=229943735.71da50e.ed75f052e06949b8884e564c3666cc95",
-        success: function (data) {
-            parseImages(data.data.map(function (x) {
-                return {
-                    url: x.images.standard_resolution.url,
-                    description: x.caption.text
-                };
-            }));
-        }
-    });
+	"use strict";
+	$.ajax({
+		type: "GET",
+		dataType: "jsonp",
+		cache: false,
+		url: "https://api.instagram.com/v1/users/self/media/recent?access_token=229943735.71da50e.ed75f052e06949b8884e564c3666cc95",
+		success: function (data) {
+			parseImages(data.data.map(function (x) {
+				return {
+					url: x.images.standard_resolution.url,
+					description: x.caption.text
+				};
+			}));
+		}
+	});
 }
 
 function parseImages(images) {
-    images.forEach(function (image) {
-        $('#photos').append('<div id="advancedImageContainer" onclick="enlargePicture(this);"><div class="detail">' + image.description + '</div><img src="' + image.url + '"/></div>');
-    })
+	images.forEach(function (image) {
+		$('#photos').append('<div id="advancedImageContainer" onclick="enlargePicture(this);"><div class="detail">' + image.description + '</div><img src="' + image.url + '"/></div>');
+	})
 
-    //A call for enlargeImage.js
-    setImageDimensions();
+	//A call for enlargeImage.js
+	setImageDimensions();
 }
 
 loadInsta();
+
+$(window).on('load', function () {
+	$('#photos').css('display', 'block');
+	$('#description').css('display', 'block');
+	$('#waitWrapper').css('display', 'none');
+})
